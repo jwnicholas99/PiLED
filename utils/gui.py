0c24aeca6388294b090e19adcb68ecac3189a904
Jing Wei Nicholas Lim <jwnicholas99@gmail.com>
2020-08-10 18:21:22 +0800

    Added tkinter.tkk Notebook with tabs

--- utils/gui.py
+++ utils/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from rpi_ws281x import Color, PixelStrip, ws
 
 class GUI():
@@ -7,20 +8,31 @@ class GUI():
 
         self.master = master
         master.title("RPi RGB")
-        master.geometry("1000x800")
+        master.geometry("800x600")
 
-        self.wave_color = tk.Label(text="What color?")
-        self.wave_color.grid(column=0, row=0)
-        self.wave_color_entry = tk.Entry()
-        self.wave_color_entry.grid(column=1, row=0)
+        self.notebook = ttk.Notebook(master)
+        self.notebook.pack()
 
-        self.wave_spread = tk.Label(text="What spread?")
-        self.wave_spread.grid(column=0, row=1)
-        self.wave_spread_entry = tk.Entry()
-        self.wave_spread_entry.grid(column=1, row=1)
+        color_wipe_frame = tk.Frame(self.notebook, width=800, height=600)
+        pulse_frame = tk.Frame(self.notebook, width=800, height=600)
+        wave_frame = tk.Frame(self.notebook, width=800, height=600)
+        rainbow_cycle_frame = tk.Frame(self.notebook, width=800, height=600)
+        rainbow_chase_frame = tk.Frame(self.notebook, width=800, height=600)
+        construct_frame = tk.Frame(self.notebook, width=800, height=600)
 
-        self.button = tk.Button(master, text="Light", command=self.light)
-        self.button.grid(column=0, row=2)
+        color_wipe_frame.pack(fill="both", expand=1)
+        pulse_frame.pack(fill="both", expand=1)
+        wave_frame.pack(fill="both", expand=1)
+        rainbow_cycle_frame.pack(fill="both", expand=1)
+        rainbow_chase_frame.pack(fill="both", expand=1)
+        construct_frame.pack(fill="both", expand=1)
+
+        self.notebook.add(color_wipe_frame, text="Color Wipe")
+        self.notebook.add(pulse_frame, text="Pulse")
+        self.notebook.add(wave_frame, text="Wave")
+        self.notebook.add(rainbow_cycle_frame, text="Rainbow Cycle")
+        self.notebook.add(rainbow_chase_frame, text="Rainbow Chase")
+        self.notebook.add(construct_frame, text="Construct")
 
     def light(self):
-        self.moodlights.wave([Color(255, 255, 255)], 255, spread=int(self.wave_spread_entry.get()))
+        self.moodlights.wave([Color(100, 150, 50)], 255, spread=int(self.wave_spread_entry.get()))
