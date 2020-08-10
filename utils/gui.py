d6e82712c8735dbb15732e7ea92e944bafee8708
Jing Wei Nicholas Lim <jwnicholas99@gmail.com>
2020-08-10 14:57:33 +0800

    Added simple Entry fields and buttons to control wave

--- utils/gui.py
+++ utils/gui.py
@@ -9,8 +9,18 @@ class GUI():
         master.title("RPi RGB")
         master.geometry("1000x800")
 
+        self.wave_color = tk.Label(text="What color?")
+        self.wave_color.grid(column=0, row=0)
+        self.wave_color_entry = tk.Entry()
+        self.wave_color_entry.grid(column=1, row=0)
+
+        self.wave_spread = tk.Label(text="What spread?")
+        self.wave_spread.grid(column=0, row=1)
+        self.wave_spread_entry = tk.Entry()
+        self.wave_spread_entry.grid(column=1, row=1)
+
         self.button = tk.Button(master, text="Light", command=self.light)
-        self.button.pack()
+        self.button.grid(column=0, row=2)
 
     def light(self):
-        self.moodlights.wave([Color(255, 255, 255)], 255, spread=7)
+        self.moodlights.wave([Color(255, 255, 255)], 255, spread=int(self.wave_spread_entry.get()))
